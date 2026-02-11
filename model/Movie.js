36056a8effc93971d879a0290aab31e77cf70a55
moxDroid Labs Inc <pritam.world@gmail.com>
2026-02-10 19:19:45 -0500

    new code added

--- model/Movie.js
+++ model/Movie.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 
 const movieSchema = mongoose.Schema({
-    mid:{
-        type: Number,
-        required: true
-    },
     name: {
         type: String,
         required: true,
